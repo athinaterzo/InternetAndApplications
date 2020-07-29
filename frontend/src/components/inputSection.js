7cedc0acc6deb33602d43b5185ea5811d3e32ef9
Athina <athina.terzoglou@gmail.com>
2020-07-29 12:53:47 +0300

    testXML

---
+++ frontend/src/components/inputSection.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class inputSection extends Component {
+    render() {
+        return (
+            <div style="background:grey; height:250px">
+                <p>sdaf</p>
+            </div>
+        )
+    }
+}
